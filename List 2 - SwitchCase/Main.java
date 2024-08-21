8937e0f2b0b2b2a26a650e06c656eee401680369
Gabriel L Soares <gabri.soares007@gmail.com>
2024-08-21 11:47:02 -0300

    Create Main.java

---
+++ List 2 - SwitchCase/Main.java
@@ -0,0 +1,66 @@
+// Criar um programa em Java com blocos independentes de código (switch case). O programa deve obedecer a seguinte ordem para cada opção numérica fornecida como entrada:
+
+// 1. Leitura e classificação de um número:
+
+// Leia um número inteiro.
+// Se o número for maior que 10, imprima "MAIOR".
+// Caso contrário, imprima "MENOR".
+// 2. Manipulação de um número:
+
+// Leia um número inteiro.
+// Se for múltiplo de 2:
+// Multiplique o número por 5.
+// Se for múltiplo de 3:
+// Subtraia 1 do número.
+// Se não for múltiplo de 2 e nem de 3:
+// Não faça nada.
+// Imprima o resultado final do número.
+// 3. Leitura de palavras:
+
+// Leia um número inteiro positivo.
+// Leia uma quantidade de palavras (String) igual ao numero informado.
+// Imprima as palavras separadas por espaço, da última para a primeira.
+// 4. Cálculo do Máximo Divisor Comum (MDC):
+
+// Leia dois números inteiros positivos.
+// Calcule o MDC entre os dois números.
+// Imprima o MDC.
+// 5. Soma de números:
+
+// Leia vários números inteiros até realizar a leitura de um não número.
+// Calcule a soma de todos os números lidos.
+// Imprima a soma final.
+// Observações:
+
+// Todas as impressões devem pular linha.
+// Caso um número seja informado ao switch case fora do intervalo permitido, o programa deve simplesmente encerrar a execução. 
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int opcao = scanner.nextInt();
+        switch (opcao) {
+            
+            case 1:
+                
+                break;
+            case 2:
+                
+                break;
+            case 3:
+                
+                break;
+            case 4:
+                
+                break;
+            case 5:
+                
+                break;
+            
+            default:
+                break;
+        }
+    }
+}
