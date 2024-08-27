d3c795a283bd7a5b2aa3acd09bd28854b1597124
Gabriel L Soares <gabri.soares007@gmail.com>
2024-08-27 11:47:12 -0300

    update list 3

--- List 2 - SwitchCase/Switch.java
+++ List 2 - SwitchCase/Switch.java
@@ -37,7 +37,7 @@
 
 import java.util.Scanner;
 
-public class Main {
+public class Switch {
     public static void main(String[] args) throws InterruptedException {
         Scanner scanner = new Scanner(System.in);
         System.out.println("\t-----MENU-----\n\n1 - Maior ou menor \n2 - Multiplo de 2 ou 3? \n3 - Organizador de palavras \n4 - MDC \n5 - Soma de números");
